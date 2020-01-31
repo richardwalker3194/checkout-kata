27bea51573844a4dfb4a1412d8f563c277086dac
Dave Lobban <dave.lobban@atmosi.com>
2020-01-31 15:39:34 +0000

    Add Basket missing from previous commit

--- checkout-kata/Basket.cs
+++ checkout-kata/Basket.cs
@@ -2,7 +2,15 @@
 
 namespace checkout_kata
 {
-    public class Class1
+    public class Basket
     {
+        public void Scan() {
+            throw new NotImplementedException("TODO");
+        }
+        public void Total()
+        {
+            throw new NotImplementedException("TODO");
+        }
     }
+
 }
