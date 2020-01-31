c2113722a09655095b3cfc04df56e6364e600369
Dave Lobban <dave.lobban@atmosi.com>
2020-01-31 10:14:48 +0000

    Initial commit

---
+++ checkout-kata.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace checkout_kata.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
