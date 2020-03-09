7252301eeb6cd4e3946c214cdd34208e83fabefd
richardwalker3194 <36680465+richardwalker3194@users.noreply.github.com>
2020-03-09 19:47:54 +0000

    Add files via upload

---
+++ Checkout2/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridViewItem = New System.Windows.Forms.DataGridView()
+        Me.ColumnItemCode = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnItem = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnQuantity = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ButtonApples = New System.Windows.Forms.Button()
+        Me.ButtonBiscuits = New System.Windows.Forms.Button()
+        Me.ButtonBakedBeans = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        CType(Me.DataGridViewItem, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridViewItem
+        '
+        Me.DataGridViewItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewItem.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColumnItemCode, Me.ColumnItem, Me.ColumnQuantity})
+        Me.DataGridViewItem.Location = New System.Drawing.Point(12, 108)
+        Me.DataGridViewItem.Name = "DataGridViewItem"
+        Me.DataGridViewItem.Size = New System.Drawing.Size(746, 150)
+        Me.DataGridViewItem.TabIndex = 0
+        '
+        'ColumnItemCode
+        '
+        Me.ColumnItemCode.DataPropertyName = "ItemCode"
+        Me.ColumnItemCode.HeaderText = "Item Code"
+        Me.ColumnItemCode.Name = "ColumnItemCode"
+        '
+        'ColumnItem
+        '
+        Me.ColumnItem.DataPropertyName = "Item"
+        Me.ColumnItem.HeaderText = "Item"
+        Me.ColumnItem.Name = "ColumnItem"
+        '
+        'ColumnQuantity
+        '
+        Me.ColumnQuantity.DataPropertyName = "Price"
+        Me.ColumnQuantity.HeaderText = "Unit Price"
+        Me.ColumnQuantity.Name = "ColumnQuantity"
+        '
+        'ButtonApples
+        '
+        Me.ButtonApples.Location = New System.Drawing.Point(13, 412)
+        Me.ButtonApples.Name = "ButtonApples"
+        Me.ButtonApples.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonApples.TabIndex = 2
+        Me.ButtonApples.Text = "Scan Apples"
+        Me.ButtonApples.UseVisualStyleBackColor = True
+        '
+        'ButtonBiscuits
+        '
+        Me.ButtonBiscuits.Location = New System.Drawing.Point(130, 412)
+        Me.ButtonBiscuits.Name = "ButtonBiscuits"
+        Me.ButtonBiscuits.Size = New System.Drawing.Size(96, 23)
+        Me.ButtonBiscuits.TabIndex = 3
+        Me.ButtonBiscuits.Text = "Scan Biscuits"
+        Me.ButtonBiscuits.UseVisualStyleBackColor = True
+        '
+        'ButtonBakedBeans
+        '
+        Me.ButtonBakedBeans.Location = New System.Drawing.Point(260, 412)
+        Me.ButtonBakedBeans.Name = "ButtonBakedBeans"
+        Me.ButtonBakedBeans.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonBakedBeans.TabIndex = 4
+        Me.ButtonBakedBeans.Text = "Scan Beans"
+        Me.ButtonBakedBeans.UseVisualStyleBackColor = True
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(12, 275)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonClear.TabIndex = 5
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.ButtonBakedBeans)
+        Me.Controls.Add(Me.ButtonBiscuits)
+        Me.Controls.Add(Me.ButtonApples)
+        Me.Controls.Add(Me.DataGridViewItem)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridViewItem, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridViewItem As DataGridView
+    Friend WithEvents ColumnItemCode As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnItem As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnQuantity As DataGridViewTextBoxColumn
+    Friend WithEvents ButtonApples As Button
+    Friend WithEvents ButtonBiscuits As Button
+    Friend WithEvents ButtonBakedBeans As Button
+    Friend WithEvents ButtonClear As Button
+End Class
---
+++ Checkout2/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Array
+Imports System.Data
+
+Public Class Form1
+
+    Dim dtItem As DataTable
+    Dim dtOffer As DataTable
+    Public Sub Startup() Handles Me.Load
+        dtItem = New DataTable
+        dtItem.Columns.Add("ItemCode", GetType(String))
+        dtItem.Columns.Add("Item", GetType(String))
+        dtItem.Columns.Add("ItemUnitPrice", GetType(Decimal))
+        dtItem.Columns.Add("ItemQuantity", GetType(Decimal))
+
+        DataGridViewItem.DataSource = dtItem
+
+        dtOffer = New DataTable
+        dtOffer.Columns.Add("ItemCode", GetType(String))
+        dtOffer.Columns.Add("ItemOfferQuantity", GetType(Decimal))
+        dtOffer.Columns.Add("ItemOfferPrice", GetType(Decimal))
+        Dim rowOffer1 As DataRow = dtOffer.NewRow()
+        rowOffer1("ItemCode") = "B15"
+        rowOffer1("ItemOfferQuantity") = 2
+        rowOffer1("ItemOfferPrice") = 0.9
+        dtOffer.Rows.Add(rowOffer1)
+        Dim rowOffer2 As DataRow = dtOffer.NewRow()
+        rowOffer2("ItemCode") = "A10"
+        rowOffer2("ItemOfferQuantity") = 3
+        rowOffer2("ItemOfferPrice") = 0.5
+        dtOffer.Rows.Add(rowOffer2)
+    End Sub
+    Public Sub PriceCalculate()
+        Dim strItemOfferCode As String
+        Dim intItemOfferQuantity As Integer
+        Dim decItemOfferPrice As Decimal
+        Dim intItemQuantity As Integer
+        For Each rowOffer As DataRow In dtOffer.Rows
+            strItemOfferCode = rowOffer("ItemCode")
+            intItemOfferQuantity = rowOffer("ItemOfferQuantity")
+            decItemOfferPrice = rowOffer("ItemOfferPrice")
+            For Each rowItem As DataRow In dtItem.Rows
+                If rowItem("ItemCode") = strItemOfferCode Then
+                    intItemQuantity += 1
+                End If
+            Next
+            If intItemQuantity >= intItemOfferQuantity Then
+                For Each row As DataRow In dtItem.Rows
+                    If row("ItemCode") = strItemOfferCode Then
+                        row("ItemUnitPrice") = decItemOfferPrice
+                    End If
+                Next
+            End If
+        Next
+    End Sub
+
+
+
+    Private Sub Apples()
+        Dim rowItem As DataRow = dtItem.NewRow()
+        rowItem("ItemCode") = "A10"
+        rowItem("Item") = "Apples"
+        rowItem("ItemUnitPrice") = 0.6
+        dtItem.Rows.Add(rowItem)
+        PriceCalculate()
+    End Sub
+
+    Private Sub Biscuits()
+        Dim rowItem As DataRow = dtItem.NewRow()
+        rowItem("ItemCode") = "B15"
+        rowItem("Item") = "Biscuits"
+        rowItem("ItemUnitPrice") = 1.0
+        dtItem.Rows.Add(rowItem)
+        PriceCalculate()
+    End Sub
+
+    Private Sub BakedBeans()
+        Dim rowItem As DataRow = dtItem.NewRow()
+        rowItem("ItemCode") = "BB11"
+        rowItem("Item") = "Baked Beans"
+        rowItem("ItemUnitPrice") = 0.9
+        dtItem.Rows.Add(rowItem)
+    End Sub
+
+    Private Sub ButtonApples_Click(sender As Object, e As EventArgs) Handles ButtonApples.Click
+        Apples()
+    End Sub
+
+    Private Sub ButtonBiscuits_Click(sender As Object, e As EventArgs) Handles ButtonBiscuits.Click
+        Biscuits()
+    End Sub
+
+    Private Sub ButtonBakedBeans_Click(sender As Object, e As EventArgs) Handles ButtonBakedBeans.Click
+        BakedBeans()
+    End Sub
+
+    Private Sub ItemClear()
+        dtItem.Rows.Clear()
+    End Sub
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        ItemClear()
+    End Sub
+End Class
